132378720abc62afc863155965b3616a7a460e4d
kieslee <kieslee@vm10-254-218-193.ksc.com>
2016-10-22 20:21:25 +0800

    use proxy

--- c_amz/settings.py
+++ c_amz/settings.py
@@ -23,12 +23,12 @@ USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:49.0) Gecko/20100
 ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+#CONCURRENT_REQUESTS = 8
 
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 1
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
@@ -53,9 +53,10 @@ COOKIES_ENABLED = False
 
 # Enable or disable downloader middlewares
 # See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    'c_amz.middlewares.MyCustomDownloaderMiddleware': 543,
-#}
+DOWNLOADER_MIDDLEWARES = {
+    'c_amz.middlewares.RotateUserAgentMiddleware': 543,
+    'c_amz.HttpProxyMiddleware.HttpProxyMiddleware': 544,
+}
 
 # Enable or disable extensions
 # See http://scrapy.readthedocs.org/en/latest/topics/extensions.html
--- c_amz/spiders/amazon.py
+++ c_amz/spiders/amazon.py
@@ -1,27 +1,40 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import re
 
 
 class AmazonSpider(scrapy.Spider):
     name = "amazon_bs"
-    allowed_domains = ["amazon.com"]
+    allowed_domains = ["amazon.com", "baidu.com"]
     start_urls = (
         'https://www.amazon.com/Best-Sellers/zgbs/ref=zg_mg_tab',
     )
 
     def parse(self, response):
+	if re.search('Robot Check', response.text):
+	    r = scrapy.Request(response.url, callback=parse)
+	    r.meta['change_proxy']=True
+	    yield r
+	    return
 	lis = response.xpath('/html/body/div[4]/div[2]/div/div[1]/div/div[2]/div/div[2]/ul/ul/li')
+	i = 0
 	for li in lis:
+	    i += 1
 	    url = response.urljoin(li.xpath('./a/@href').extract_first())
 	    class_name = li.xpath('./a/text()').extract_first()
 	    #yield {'url': url, 'class_name': class_name}
 	    r = scrapy.Request(url, callback=self.parse_class_pages_first)
 	    r.meta['class_name'] = class_name
 	    yield r
-	    break
-	    
+	    if i > 2:
+		break
 
     def parse_class_pages_first(self, response):
+	if re.search('Robot Check', response.text):
+	    r = scrapy.Request(response.url, callback=parse_class_pages_first)
+	    r.meta['change_proxy']=True
+	    yield r
+	    return
         class_name = response.meta['class_name']
 	divs = response.xpath('//*[@id="zg_centerListWrapper"]/div[contains(@class, "zg_itemImmersion")]')
 	for div in divs:
@@ -32,6 +45,8 @@ class AmazonSpider(scrapy.Spider):
 	    r.meta['rank'] = rank
 	    yield r
 	
+	return 
+
 	page2 = response.xpath('//*[@id="zg_page2"]/a/@href').extract_first()
 	r = scrapy.Request(page2, callback=self.parse_class_pages)
 	r.meta['class_name'] = class_name
@@ -54,12 +69,17 @@ class AmazonSpider(scrapy.Spider):
 
 
     def parse_class_pages(self, response):
+	if re.search('Robot Check', response.text):
+	    r = scrapy.Request(response.url, callback=parse_class_pages, dont_filter=True)
+	    r.meta['change_proxy']=True
+	    yield r
+	    return
         class_name = response.meta['class_name']
 	divs = response.xpath('//*[@id="zg_centerListWrapper"]/div[contains(@class, "zg_itemImmersion")]')
 	for div in divs:
 	    url = div.xpath('./div/div[2]/a/@href').extract_first().strip('\n')
 	    rank = div.xpath('./div/span/text()').extract_first().strip('.')
-	    r = scrapy.Request(url, callback=self.parse_products)
+	    r = scrapy.Request(url, callback=self.parse_products, dont_filter=True)
 	    r.meta['class_name'] = class_name
 	    r.meta['rank'] = rank
 	    yield r
@@ -68,15 +88,23 @@ class AmazonSpider(scrapy.Spider):
         class_name = response.meta['class_name']
 	rank = response.meta['rank']
 	
-	name = response.xpath('//*[@id="productTitle"]/text()').extract_first().strip('\n          ')
-	price = response.xpath('//*[@id="priceblock_ourprice"]/text()').extract_first()
-	if price is None:
-	    price = response.xpath('//*[@id="priceblock_saleprice"]/text()').extract_first()
-	if price is None:
-	    price = 'Currently unavailable'
-	stars = response.xpath('//*[@id="reviewStarsLinkedCustomerReviews"]/i/span/text()').extract_first()
-	if stars is None:
-	    stars = 'Not Found'
+	try:
+	    name = response.xpath('//*[@id="productTitle"]/text()').extract_first().strip('\n          ')
+	    price = response.xpath('//*[@id="priceblock_ourprice"]/text()').extract_first()
+	    if price is None:
+	        price = response.xpath('//*[@id="priceblock_saleprice"]/text()').extract_first()
+	    if price is None:
+	        price = 'Currently unavailable'
+	    stars = response.xpath('//*[@id="reviewStarsLinkedCustomerReviews"]/i/span/text()').extract_first()
+	    if stars is None:
+	        stars = 'Not Found'
 
-	yield {'class_name': class_name, 'rank': rank, 'name': name, 'price': price, 'stars': stars, 'url': response.url}
-	
+	    yield {'class_name': class_name, 'rank': rank, 'name': name, 'price': price, 'stars': stars, 'url': response.url}
+	except Exception, e:
+	    r = response.request
+	    #r = scrapy.Request(response.url, callback=self.parse_products, dont_filter=True)
+	    r.dont_filter = True
+	    r.meta['change_proxy'] = True
+	    #r.meta['class_name'] = class_name
+	    #r.meta['rank'] = rank
+	    yield r
